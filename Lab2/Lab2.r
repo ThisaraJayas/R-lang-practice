e0e4121f558933ced3669a4630e6f9ab467b35d2
Thisara <sasmithajayasinghe1@gmail.com>
2024-02-03 17:48:01 +0530

    e

---
+++ Lab2/Lab2.r
@@ -0,0 +1,43 @@
+
+#if else---
+
+x<-4  #4
+y<--3 #-3
+
+if(x>0){
+  print("Greater than 0")
+}else{
+  print("Not Greater")
+}
+
+#nested if-else----------
+
+x<-3
+
+if(x>0){
+  print("Greater than 0")
+}else if(x<0){
+  print("Negative Number")
+}else{
+  print("X is 0")
+}
+
+#while loop-----------
+
+i<-1
+
+while(i<6){
+  print(i)
+  i=i+1
+}
+
+#for--------------
+#5:10
+
+for(i in 1:10){
+  print(i)
+}
+
+
+#read.table (from txt file)
+#read.csv()  (from csv file)
