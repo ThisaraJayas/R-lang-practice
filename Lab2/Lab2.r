f362a43ccb8ad67f0eeae2368740f4efcd3809da
Thisara <sasmithajayasinghe1@gmail.com>
2024-02-03 21:38:10 +0530

    commmits

--- Lab2/Lab2.r
+++ Lab2/Lab2.r
@@ -41,3 +41,35 @@ for(i in 1:10){
 
 #read.table (from txt file)
 #read.csv()  (from csv file)
+
+data<-read.csv("DATA 2.csv")
+fix(data) #open data edditor mode
+data
+
+#read.table("data.txt, header=TRUE, sep="," )
+
+#------------------------------------------------------------
+#Write data into a file---
+#write.csv()  or write.table()
+
+index<-c(1,2,3)
+Name<-c("Kamal","Nimal","Sunil")
+marks<-c(34,64,75)
+
+dataframe<-data.frame(index,Name,marks)
+dataframe
+
+#index   Name   marks
+#  1    Kamal    34
+#.....
+
+#save data into csv file
+write.csv(dataframe, "dataframe1.csv")
+
+#save data into txt file
+write.table(dataframe, "dataframe2.txt")
+
+###########################  FUNCTIONS  ##################################
+
+
+
